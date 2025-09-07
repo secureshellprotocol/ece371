6e75f25bb983e1c1499e79dbd2f835784673f295
JAMES RYAN <james.paul9889@gmail.com>
2025-09-07 18:01:24 -0400

    feat: added wick chimes on the minute, added recharge for hour rollover
    12->1 causes all wicks to "recharge" and descend from the top

--- project1/sketch.js
+++ project1/sketch.js
@@ -3,6 +3,15 @@ let config = {
     HEIGHT: 900
 };
 
+let state = {
+    minute_change_chime: 0,
+    minute_change_opacity: 150,
+    recharge: 0
+};
+
+// maps hour -> number of active wicks
+const staticWickMap = [1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
 // setup() is called once at page-load
 function setup() {
     createCanvas(config.WIDTH, config.HEIGHT);
@@ -10,47 +19,70 @@ function setup() {
 
 // draw() is called 60 times per second
 function draw() {
-    let hr = hour() % 12;
+    let hr = (hour() % 12);
     let min = minute();
     let sec = second();
 
-    if (sec == 0) {
-        console.log("MINUTE " + min);
-    }
-
-    background(225);
+    background(50);
 
     drawClockState(hr, min, sec);
-    //textSize(32);
-    //fill(180);
-    //text(hr, 10, 30);
-    //fill(100);
-    //text(min, 10, 60);
-    //fill(0);
-    //text(sec, 10, 90);
 }
 
-// Given a time, draw a specific state of the wicks
+// Given a time, draw a specific state of the wicks.
 function drawClockState(hr, min, sec) {
     // Number of active wicks, including the currently burning one
-    let numActive = 12 - hr + 1;
+    let numActive = staticWickMap[hr];
+
+    // If its a new minute, prep the chime!
+    if (sec == 0 && state.minute_change_chime == 0) {
+        console.log("MINUTE "+ min);
+        state.minute_change_chime = 1;
+    }
+
+    // if we're rolling over from hr 12 -> 1, 'recharge'
+    if (numActive == 12 && min == 0 && sec == 0) {
+        state.recharge = 450;
+        return
+    }
+
+    // the end of a wick 'chimes' for one second when we go to the next minute
+    if (state.minute_change_chime == 1) {
+        // over one second, fade out to zero opacity. This implicitly relies on
+        // this function being called 60 times a second!
+        state.minute_change_opacity -= (150 / 60);
+
+        // after the second passes, disable the chime and reset
+        if (sec == 1) {
+            state.minute_change_chime = 0;
+            state.minute_change_opacity = 150;
+        }
+    }
 
     // Draw 12 evenly spaced wicks right-to-left
     for (let w = 0; w < numActive; w++) {
         // Calculate the offset from the left. We account for the total space
         // each candle takes, adding an offset to place it roughly in the center
         let offsetPercent = 100 * ( (12-w) / 12);
-        if (w == numActive-1) {
-            drawWick(min, sec, offsetPercent);
-        } else {
-            drawWick(0, 0, offsetPercent);
+
+        if (state.recharge == 0) {
+            if (w == numActive-1) {   // last wick, getting 'consumed'
+                drawWick(min, sec, offsetPercent, 1);
+            } else {
+                drawWick(0, 0, offsetPercent, 0);
+            }
+        } else {    // recharge routine
+            state.minute_change_chime = 1;
+            drawWick(((state.recharge / 5) | 0), 0, offsetPercent, 1);
+            state.recharge--;
         }
+
     }
     console.log("drew " +numActive+" wicks.");
 }
 
-// Draws one wick at some percent offset from the left.
-function drawWick(min, sec, horizOffsetPercent) {
+// Draws one wick at some percent offset from the left. if we are 'noisy', we
+// chime if it's a new minute.
+function drawWick(min, sec, horizOffsetPercent, noisy) {
     // convert relative measurements to pixels
     // place a 10% buffer on both sides of the canvas, then calculate the offset
     let buffer = config.WIDTH * 0.1
@@ -59,7 +91,16 @@ function drawWick(min, sec, horizOffsetPercent) {
     // calculate how much of the wick remains, in percent
     let percentRemaining = 1 - (((60 * min) + (sec)) / 3600);
 
-    let c = color(51, 51, 0);
+    // chime if we have to
+    if (state.minute_change_chime == 1 && noisy == 1) {
+        let bell_color = color(191, 191, 0);
+        bell_color.setAlpha(state.minute_change_opacity);
+        fill(bell_color);
+        circle(buffer + offset, config.HEIGHT * 0.9 * percentRemaining, 30);
+    }
+
+    // draw the wick
+    let c = color(225, 225, 225);
     fill(c);
     noStroke();
     rect(buffer + offset - 8, 0, 16, config.HEIGHT * 0.9 * percentRemaining);
